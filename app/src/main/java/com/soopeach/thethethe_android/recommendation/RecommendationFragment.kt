c4d0c308de73aa48c4ba558fe62f27ed21e5329c
soopeach <hsjeon584@gmail.com>
2024-01-12 23:27:00 +0900

    비디오 가져와서 화면에 보여주는 기능 구현

--- app/src/main/java/com/soopeach/thethethe_android/recommendation/RecommendationFragment.kt
+++ app/src/main/java/com/soopeach/thethethe_android/recommendation/RecommendationFragment.kt
@@ -7,9 +7,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.soopeach.thethethe_android.data.network.NetworkModule
 import com.soopeach.thethethe_android.databinding.FragmentRecommendationBinding
-import com.soopeach.thethethe_android.model.RecommendationVideo
+import kotlinx.coroutines.launch
 
 class RecommendationFragment : Fragment() {
 
@@ -41,25 +43,14 @@ class RecommendationFragment : Fragment() {
         binding.recommendationVideoRecyclerView.layoutManager =
             LinearLayoutManager(requireContext())
 
-        adapter.submitList(
-            listOf(
-                RecommendationVideo(
-                    "-KLXcO_BZ8A",
-                    "거북목 교정 운동, 이거 하나로 끝낸다! - 치맥 운동",
-                    "https://i.ytimg.com/vi/-KLXcO_BZ8A/mqdefault.jpg"
-                ),
-                RecommendationVideo(
-                    "0D6fn3i2-e0",
-                    "거북목-일자목-뒷목-어깨 통증 간단하게 해결하는 방법",
-                    "https://i.ytimg.com/vi/0D6fn3i2-e0/mqdefault.jpg"
-                ),
-                RecommendationVideo(
-                    "1g6vB2O4qZQ",
-                    "[Ep.09] 잘 낫지 않는 거북목 \uD83D\uDE31 올바른 스트레칭으로 교정하세요! - 안좋은 자세, 습관, 직업 등으로 형성된 거북목은 만성적인 두통, 목통증의 원인이 됩니다!",
-                    "https://i.ytimg.com/vi/1g6vB2O4qZQ/mqdefault.jpg"
+        viewLifecycleOwner.lifecycleScope.launch {
+            NetworkModule.getVideoApi().getVideos().apply {
+                adapter.submitList(
+                    this
                 )
-            )
-        )
+            }
+        }
+
 
     }
 
