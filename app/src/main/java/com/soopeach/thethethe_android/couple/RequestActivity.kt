c3ae3c6264d7a6ed0aaa80038720272acb2e9388
kangsujung <rkdtnwjd1030@naver.com>
2024-01-12 20:42:07 +0900

    bottonbar 생성

---
+++ app/src/main/java/com/soopeach/thethethe_android/couple/RequestActivity.kt
@@ -0,0 +1,12 @@
+package com.soopeach.thethethe_android.couple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.soopeach.thethethe_android.R
+
+class RequestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request)
+    }
+}
