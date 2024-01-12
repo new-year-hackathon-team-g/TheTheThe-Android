3967456e8fe2fa34f46d9cf168d82dfbdedcc4ec
soopeach <hsjeon584@gmail.com>
2024-01-13 05:17:24 +0900

    CoupleRequest 모델 추가

---
+++ app/src/main/java/com/soopeach/thethethe_android/model/couple/CoupleRequest.kt
@@ -0,0 +1,8 @@
+package com.soopeach.thethethe_android.model.couple
+
+data class CoupleRequest(
+    val coupleName: String,
+    val coupleImageUrl: String,
+    val introduction: String,
+    val startDate: String,
+)
